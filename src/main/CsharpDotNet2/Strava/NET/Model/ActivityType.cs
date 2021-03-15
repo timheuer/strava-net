93a898ae7065bb007c9c684f6e47fee09b6c3ac8
David Synott <david.synott@fixflo.com>
2021-03-15 23:52:57 +0000

    change activity type to enum and copy in all the enums from the Strava API docs.

--- src/main/CsharpDotNet2/Strava/NET/Model/ActivityType.cs
+++ src/main/CsharpDotNet2/Strava/NET/Model/ActivityType.cs
@@ -1,36 +1,43 @@
-using System;
-using System.Text;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
-using Newtonsoft.Json;
-
-namespace Strava.NET.Model {
-
-  /// <summary>
-  /// An enumeration of the types an activity may have.
-  /// </summary>
-  [DataContract]
-  public class ActivityType {
-
-    /// <summary>
-    /// Get the string presentation of the object
-    /// </summary>
-    /// <returns>String presentation of the object</returns>
-    public override string ToString()  {
-      var sb = new StringBuilder();
-      sb.Append("class ActivityType {\n");
-      sb.Append("}\n");
-      return sb.ToString();
+namespace Strava.NET.Model
+{
+    public enum ActivityType
+    {
+        AlpineSki,
+        BackcountrySki,
+        Canoeing,
+        Crossfit,
+        EBikeRide,
+        Elliptical,
+        Golf,
+        Handcycle,
+        Hike,
+        IceSkate,
+        InlineSkate,
+        Kayaking,
+        Kitesurf,
+        NordicSki,
+        Ride,
+        RockClimbing,
+        RollerSki,
+        Rowing,
+        Run,
+        Sail,
+        Skateboard,
+        Snowboard,
+        Snowshoe,
+        Soccer,
+        StairStepper,
+        StandUpPaddling,
+        Surfing,
+        Swim,
+        Velomobile,
+        VirtualRide,
+        VirtualRun,
+        Walk,
+        WeightTraining,
+        Wheelchair,
+        Windsurf,
+        Workout,
+        Yoga
     }
-
-    /// <summary>
-    /// Get the JSON string presentation of the object
-    /// </summary>
-    /// <returns>JSON string presentation of the object</returns>
-    public string ToJson() {
-      return JsonConvert.SerializeObject(this, Formatting.Indented);
-    }
-
-}
 }
